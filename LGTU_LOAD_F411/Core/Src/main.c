be2dd772da787e984319712921a4af5b9fe2c5e7
vik <yurchenkovik@yundex.ru>
2025-04-25 21:36:41 +0300

    Добавил библиотеку DSP

--- LGTU_LOAD_F411/Core/Src/main.c
+++ LGTU_LOAD_F411/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "arm_math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/

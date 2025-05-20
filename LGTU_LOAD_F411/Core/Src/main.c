81b29bec9370376a1495796c09d7454c3de819e7
vik <yurchenkovik@yundex.ru>
2025-05-20 11:37:16 +0300

    Использовал тернарный оператор

--- LGTU_LOAD_F411/Core/Src/main.c
+++ LGTU_LOAD_F411/Core/Src/main.c
@@ -64,40 +64,39 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_ADC1_Init();
-  MX_TIM1_Init();
-  MX_TIM2_Init();
-  MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_I2C1_Init();
+	MX_ADC1_Init();
+	MX_TIM1_Init();
+	MX_TIM2_Init();
+	MX_USART1_UART_Init();
+	/* USER CODE BEGIN 2 */
 	HAL_TIM_Encoder_Start_IT(&htim1, TIM_CHANNEL_ALL); // Запускаем энкодер в режиме прерывания и запускаем каналы таймера 1
 	//запуск экрана:
 	ssd1306_Init();
@@ -108,74 +107,69 @@ int main(void)
 	//upd_chisl(0.0, 3);
 	//upd_chisl(15.0, 2);
 	//upd_chisl(23.8, 3);
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
-
-
 
 		if (button_flag) {
 			Button_click_process(); // Если полнялся флажок прерывания кнопки энкодера, выполняем функцию
 		}
-		change_screen (long_press); // опрашиваем смену экрана по длительному нажатию кнопки энкодера
+		change_screen(long_press); // опрашиваем смену экрана по длительному нажатию кнопки энкодера
 		if (mode_item == 2) {
-		draw_blinking_underline_disch(menu_item_disch); // Функция для реализации моргания и статичного подчеркивания изменяемых энкодером значений
+			draw_blinking_underline_disch(menu_item_disch); // Функция для реализации моргания и статичного подчеркивания изменяемых энкодером значений
 		}
-	    if (mode_item == 1) {
-		draw_blinking_underline_load(menu_item_load);
+		if (mode_item == 1) {
+			draw_blinking_underline_load(menu_item_load);
 		}
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 12;
-  RCC_OscInitStruct.PLL.PLLN = 96;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 12;
+	RCC_OscInitStruct.PLL.PLLN = 96;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -189,93 +183,103 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 // Функция для обработки прерывания энкодера
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
-    static uint16_t prev_counter_encoder = 0; // Переменная для хранения предыдущего значения counter_encoder
-    int16_t encoder_diff = 0; // Переменная которая хранит разницу текущего и предыдущего значения
+	static uint16_t prev_counter_encoder = 0; // Переменная для хранения предыдущего значения counter_encoder
+	int16_t encoder_diff = 0; // Переменная которая хранит разницу текущего и предыдущего значения
 
-    if (htim->Instance == TIM1) { // Проверяем, что прерывание пришло от таймера 1
+	if (htim->Instance == TIM1) { // Проверяем, что прерывание пришло от таймера 1
 
-    	if (long_press == 0) {
-    	encoder_diff = TIM1->CNT - prev_counter_encoder; // Вычисляем разницу между текущим и предыдущим значением счетчика
-        counter_encoder = TIM1->CNT; // текущее значение счетчика
-        }
+		if (long_press == 0) {
+			encoder_diff = TIM1->CNT - prev_counter_encoder; // Вычисляем разницу между текущим и предыдущим значением счетчика
+			counter_encoder = TIM1->CNT; // текущее значение счетчика
+		}
 
-    	if (short_press == 0 && long_press == 0 && mode_item == 1) {
-    	    menu_item_load = (counter_encoder >> 1) % 3U + 1U; // Вычисляем выбранный пункт меню (1-3)
-    	}
+		if (short_press == 0 && long_press == 0 && mode_item == 1) {
+			menu_item_load = (counter_encoder >> 1) % 3U + 1U; // Вычисляем выбранный пункт меню (1-3)
+		}
 
-        if (short_press == 0 && long_press == 0 && mode_item == 2) {
-            menu_item_disch = (counter_encoder >> 1) % 4U + 1U; // Вычисляем выбранный пункт меню (1-4)
-        }
+		if (short_press == 0 && long_press == 0 && mode_item == 2) {
+			menu_item_disch = (counter_encoder >> 1) % 4U + 1U; // Вычисляем выбранный пункт меню (1-4)
+		}
 
+		if (short_press == 1 && long_press == 0 && mode_item == 2) {
+			switch (menu_item_disch) {
+			case 1:
+				mode_item = (counter_encoder >> 1) % 2U + 1U;
+				start_screen(mode_item);
+				upd_mode(mode_item);
+				break;
+			case 2:
+				type_item = (counter_encoder >> 1) % 4U + 1U;
+				upd_type(type_item);
+				break;
+			case 3:
+				voltage_value += 0.1f * (encoder_diff >> 1);
+				if (voltage_value > 15.0f)
+					voltage_value = 15.0f;
+				if (voltage_value < 0.0f)
+					voltage_value = 0.0f;
+				upd_chisl(voltage_value, 5);
+				break;
+			case 4:
+				current_value += 0.1f * (encoder_diff >> 1);
+				if (current_value > 10.0f)
+					current_value = 10.0f;
+				if (current_value < 0.0f)
+					current_value = 0.0f;
+				upd_chisl(current_value, 4);
+				break;
+			}
+		}
+		if (short_press == 1 && long_press == 0 && mode_item == 1) {
+			switch (menu_item_load) {
+			case 1:
+				mode_item = (counter_encoder >> 1) % 2U + 1U;
+				start_screen(mode_item);
+				upd_mode(mode_item);
+				break;
+			case 2:
+				voltage_value += 0.1f * (encoder_diff >> 1);
+				if (voltage_value > 15.0f)
+					voltage_value = 15.0f;
+				if (voltage_value < 0.0f)
+					voltage_value = 0.0f;
+				upd_chisl(voltage_value, 3);
+				break;
+			case 3:
+				current_value += 0.1f * (encoder_diff >> 1);
 
-        if (short_press == 1 && long_press == 0 && mode_item == 2) {
-                switch (menu_item_disch) {
-                    case 1:
-                        mode_item = (counter_encoder >> 1) % 2U + 1U;
-                        start_screen(mode_item);
-                        upd_mode(mode_item);
-                        break;
-                    case 2:
-                        type_item = (counter_encoder >> 1) % 4U + 1U;
-                        upd_type(type_item);
-                        break;
-                    case 3:
-                        voltage_value += 0.1f * (encoder_diff >> 1);
-                        if (voltage_value > 15.0f) voltage_value = 15.0f;
-                        if (voltage_value < 0.0f) voltage_value = 0.0f;
-                        upd_chisl(voltage_value, 5);
-                        break;
-                    case 4:
-                        current_value += 0.1f * (encoder_diff >> 1);
-                        if (current_value > 10.0f) current_value = 10.0f;
-                        if (current_value < 0.0f) current_value = 0.0f;
-                        upd_chisl(current_value, 4);
-                        break;
-                }
-            }
-        if (short_press == 1 && long_press == 0 && mode_item == 1) {
-                        switch (menu_item_load) {
-                            case 1:
-                                mode_item = (counter_encoder >> 1) % 2U + 1U;
-                                start_screen(mode_item);
-                                upd_mode(mode_item);
-                                break;
-                            case 2:
-                            voltage_value += 0.1f * (encoder_diff >> 1);
-                            if (voltage_value > 15.0f) voltage_value = 15.0f;
-                            if (voltage_value < 0.0f) voltage_value = 0.0f;
-                            upd_chisl(voltage_value, 3);
-                            break;
-                           case 3:
-                             current_value += 0.1f * (encoder_diff >> 1);
-                            if (current_value > 10.0f) current_value = 10.0f;
-                            if (current_value < 0.0f) current_value = 0.0f;
-                            upd_chisl(current_value, 2);
-                            break;
+				/*
+				if (current_value > 10.0f)
+					current_value = 10.0f;
+				*/
+				current_value = (current_value > 10.0f) ? 10.f : current_value;
+				current_value = (current_value < 0.0f) ? 0 : current_value;
+				/*
+				if (current_value < 0.0f)
+					current_value = 0.0f;
+				*/
+				upd_chisl(current_value, 2);
+				break;
 
-                        }
-        }
-        prev_counter_encoder = counter_encoder; // Сохраняем текущее значение для следующего вызова
+			}
+		}
+		prev_counter_encoder = counter_encoder; // Сохраняем текущее значение для следующего вызова
+	}
 }
-}
-
-
-
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
